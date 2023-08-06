e84181c4e383ce44c584e8359f16ff3fbe0757b3
LuaMartz <luisamartinez030503@gmail.com>
2023-08-05 23:04:50 -0500

    Items added Changes to be committed: 	new file:   .hintrc 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/item/item.component.css 	modified:   src/app/item/item.component.html 	modified:   src/app/item/item.component.ts 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,46 @@ import { Item } from './item';
   styleUrls: ['./app.component.css']
 })
 
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+
+
 export class AppComponent {
-addItem(arg0: string) {
-throw new Error('Method not implemented.');
-}
+  addItem(description: string) {  // Fixed parameter name
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
   title = 'todo';
 
   // ToDo: Eliminar esta lista al momento de integrar la API
-  filter: "all" | "active" | "done" = "all"
+  filter: "all" | "active" | "done" = "all";
 
   allItems = [
-    {description: "Diseño de personajes Nacho", done:false},
-    {description: "Desarrollo página Laboratorio", done:false},
-    {description: "Storyboard Sena", done:false},
-    {description: "Marca Chromatic Cakes", done:false},
-    {description: "Curso Photoshop", done:true},
-    {description: "Curso Illustrator", done:true},
-  ]
+    { description: "Diseño de personajes Nacho", done: false },
+    { description: "Desarrollo página Laboratorio", done: false },
+    { description: "Storyboard Sena", done: false },
+    { description: "Marca Chromatic Cakes", done: false },
+    { description: "Curso Photoshop", done: true },
+    { description: "Curso Illustrator", done: true },
+  ];
 
   get items() {
-    if (this.filter === "all"){
-      return this.allItems
+    if (this.filter === "all") {
+      return this.allItems;
     }
-    return this.allItems.filter((item)=>{
-      this.filter ==="done" ? item.done : !item.done
-    })
+    return this.allItems.filter((item) => {
+      return this.filter === "done" ? item.done : !item.done;  // Added return statement
+    });
   }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
 }
